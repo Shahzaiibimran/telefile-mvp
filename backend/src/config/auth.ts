2d144dc39968cb69d0675ef5ac67ab56e1d36bfc
Shahzaiibimran <softianzshahzaib@gmail.com>
2025-05-08 17:37:08 +0500

    fix | custom types

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -2,7 +2,6 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import dotenv from 'dotenv';
 import User, { IUser } from '../models/User';
-import '../types/express';
 
 dotenv.config();
 
